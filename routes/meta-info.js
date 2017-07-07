3ce2c99d5ac954bc4499d273c7021c7968e0999f
Amitesh Kumar <amitesh.kumar@mastercard.com>
2017-07-07 15:39:26 +0530

    Fetching meta data on startup

--- routes/meta-info.js
+++ routes/meta-info.js
@@ -4,23 +4,39 @@ var router = express.Router();
 var models = require('../models');
 
 /* GET meta info listing. */
-router.get('/', function(req, res, next) {
-    models.metaInfo.findAll({limit: 1}).then(function(metaInfo) {
-        res.json(formatMeta(metaInfo));
-    });
+router.get('/', function (req, res, next) {
+    models.metaInfo.findAll({limit: 1, attributes: ['streams', 'roles', 'participated', 'status', 'hiringStatus']})
+        .then(function success(metaInfo) {
+
+            models.drive.findAll({attributes: ['id', 'scheduledOn', 'name']}).then(function success(drives) {
+
+                models.testType.findAll({
+                    attributes: ['id', 'order', 'name'],
+                    order: [['order']]
+                }).then(function success(testTypes) {
+                    var data = {
+                        metaInfo: formatMeta(metaInfo),
+                        drives: drives,
+                        testTypes: testTypes
+                    };
+
+                    res.json(data);
+                });
+            });
+        });
 });
 
-function formatMeta(meta){
-  var formatedMeta = {};
-  if(meta.length > 0){
-      var info = meta[0];
-      console.log(info);
-      formatedMeta.streams = _.split(info.streams, ',');
-      formatedMeta.roles = _.split(info.roles, ',');
-      formatedMeta.participated = _.split(info.participated, ',');
-      formatedMeta.status = _.split(info.status, ',');
-      formatedMeta.hiringStatus = _.split(info.hiringStatus, ',');
-  }
-  return formatedMeta;
+function formatMeta(meta) {
+    var formatedMeta = {};
+    if (meta.length > 0) {
+        var info = meta[0];
+        console.log(info);
+        formatedMeta.streams = _.split(info.streams, ',');
+        formatedMeta.roles = _.split(info.roles, ',');
+        formatedMeta.participated = _.split(info.participated, ',');
+        formatedMeta.status = _.split(info.status, ',');
+        formatedMeta.hiringStatus = _.split(info.hiringStatus, ',');
+    }
+    return formatedMeta;
 }
 module.exports = router;
